ba8dffcf65afbd249fe495e1adf82a7bd283dd4a
Raphael Croce <rcroce7@gmail.com>
2019-03-10 16:38:56 -0500

    Added logic to fetch albums from api and rendering each as album detail

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import axios from 'axios';
+
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
+  state = {
+    albums: []
+  }
 
   componentWillMount() {
-    console.log('HELLO');
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then((response) => {
+        console.log("THIS IS THE RESPONSE FROM API", response);
+        this.setState({
+          albums: response.data
+        });
+
+        // console.log('THIS IS THE NEW STATE', this.state);
+      });
+  }
+
+  renderAlbums() {
+    // console.log("THIS IS THE STATE", this.state)
+    return this.state.albums.map((album) => 
+      <AlbumDetail key={album.title} albums={album}/>
+    )
   }
 
   render() {
     return (
       <View>
-        <Text>Album List</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
